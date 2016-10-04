61bba660b48fea38763b4a3c2186060acee5705b
Nordine59 <itounour@hotmail.fr>
2016-10-04 17:02:18 +0200

    Derniere version04-10-1614h39

---
+++ StreamingMavenSpringBoot/src/main/java/streaming/dao/PaysDao.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package streaming.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import static streaming.entity.Film_.pays;
+import streaming.entity.Pays;
+
+/**
+ *
+ * @author admin
+ */
+
+@Repository
+public class PaysDao {
+    
+    @PersistenceContext
+    private EntityManager em;
+    
+    
+    @Transactional
+    public void ajouter (Pays pays){
+        
+        em.persist(pays);
+        
+    }
+    
+    
+    
+    public Pays rechercher(long id){
+        
+        return em.find(Pays.class, id);
+        
+    }
+    
+}
